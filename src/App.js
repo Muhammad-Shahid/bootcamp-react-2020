3c6b615e72f3c2f910eb7bedc8eecebb47d96ea9
faaz <faaz@localhost.localdomain>
2020-06-08 14:41:34 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome to faaz's react app 2020
+          welcome to faaz's react app 2020 with crona
         </p>
         <a
           className="App-link"
